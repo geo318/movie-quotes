549aed97bbebd533fd55f6c17fa7350ce2cc891d
geo318 <geo.lomidze@gmail.com>
2023-02-11 01:18:35 +0400

    fix: time helper function

--- helpers/getTime.ts
+++ helpers/getTime.ts
@@ -1,19 +1,20 @@
 const times = {
-  days: { en: 'days', ka: 'დღე' },
-  hours: { en: 'hours', ka: 'საათი' },
-  minutes: { en: 'minutes', ka: 'წუთი' },
+  days: { en: 'days', ka: 'დღის' },
+  hours: { en: 'hours', ka: 'საათის' },
+  minutes: { en: 'minutes', ka: 'წუთის' },
   seconds: { en: 'seconds', ka: 'წამების' },
 };
 
 export const getTimeElapsed = (time: string, locale: 'en' | 'ka') => {
   const now = new Date();
   const event = new Date(time);
-  const t = (key: 'getTime' | 'getHours' | 'getMinutes' | 'getSeconds') => {
+  const t = (
+    key: 'getDay' | 'getTime' | 'getHours' | 'getMinutes' | 'getSeconds'
+  ) => {
     return now[key]() - event[key]();
   };
   return (
-    (t('getHours') / 24 >= 1 &&
-      `${(t('getHours') / 24).toFixed()} ${times.days[locale]}`) ||
+    (t('getDay') && `${t('getDay')} ${times.days[locale]}`) ||
     (t('getHours') && `${t('getHours')} ${times.hours[locale]}`) ||
     (t('getMinutes') && `${t('getMinutes')} ${times.minutes[locale]}`) ||
     (t('getSeconds') && `${times.seconds[locale]}`)
